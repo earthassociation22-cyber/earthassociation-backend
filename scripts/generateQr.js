9c9ec708cd51839634805e2e25ddc8c62de8e166
Yuvraj <yuvrajbhatiofficial@gmail.com>
2026-05-16 23:34:35 +0530

    configure backend url

--- scripts/generateQr.js
+++ scripts/generateQr.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const targetDomain = process.env.FRONTEND_URL || 'https://domain.com';
+const targetDomain = process.env.FRONTEND_URL || 'https://www.earthassociation.in';
 const verifyUrl = `${targetDomain}/verify`;
 
 const publicDir = path.join(__dirname, '../../EarthAssociation/public');
